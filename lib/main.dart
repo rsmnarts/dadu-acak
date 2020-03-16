74888c7db96d49426961f220af4e0337f6598cc0
rsmnarts <rsmnart@gmail.com>
2020-03-17 01:03:20 +0700

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.teal,
         appBar: AppBar(
           title: Text('Dadu Acak'),
-          backgroundColor: Colors.red,
+          centerTitle: true,
+          backgroundColor: Colors.teal,
         ),
         body: DicePage(),
       ),
@@ -15,22 +17,40 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _State createState() => _State();
+}
+
+class _State extends State<DicePage> {
+  int leftDiceNumber = 6, rightDiceNumber = 6;
+
+  void acakDadu() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice1.png'),
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () => acakDadu(),
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
           ),
-        ),
-        Expanded(
-          child: Image(
-            image: AssetImage('images/dice1.png'),
+          Expanded(
+            child: FlatButton(
+              onPressed: () => acakDadu(),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
